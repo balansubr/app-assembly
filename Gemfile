c8822613084cc274ed8729f9a47ba30510cd5b38
Balan Subramanian <balan@heroku.com>
2014-04-08 18:00:59 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "https://rubygems.org"
+
+gem "excon"
+gem "multi_json"
+gem "omniauth-heroku"
+gem "sinatra"
+gem "thin"
---
+++ web.rb
@@ -0,0 +1,52 @@
+$stdout.sync = $stderr.sync = true
+
+require "cgi"
+require "excon"
+require "multi_json"
+require "sinatra"
+require "omniauth"
+require "omniauth-heroku"
+
+use Rack::Session::Cookie, :secret => ENV["COOKIE_SECRET"]
+use OmniAuth::Builder do
+  provider :heroku, ENV["HEROKU_OAUTH_ID"], ENV["HEROKU_OAUTH_SECRET"], { scope: "identity" }
+end
+
+get "/" do
+  <<-HTML
+    <a href='/auth/heroku'>Sign in with Heroku</a>
+  HTML
+end
+
+get "/auth/heroku/callback" do
+  session[:heroku_oauth_token] =
+    request.env["omniauth.auth"]["credentials"]["token"]
+  redirect "/user"
+end
+
+get "/user" do
+  if !session[:heroku_oauth_token]
+    redirect("/")
+  else
+    api = Excon.new(ENV["HEROKU_API_URL"] || "https://api.heroku.com",
+      headers: { "Authorization" => "Bearer #{session[:heroku_oauth_token]}" },
+      ssl_verify_peer: ENV["SSL_VERIFY_PEER"] != "false")
+    res = api.get(path: "/account", expects: 200)
+    user_email = MultiJson.decode(res.body)["email"]
+
+    <<-HTML
+      Hi #{CGI.escapeHTML(user_email)}
+    HTML
+  end
+end
+
+get "/getting-started" do
+  <<-HTML
+    Try this app:
+    <ol>
+      <li>Go to the provisioned app's URL</li>
+      <li>Log into a different heroku ID</li>
+      <li>See the email address</li>
+    </ol>
+  HTML
+end

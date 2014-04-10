72d198dadecf0adedb89874f98dcced65b2be838
Balan Subramanian <balan@heroku.com>
2014-04-09 17:17:46 -0700

    initial full code

--- web.rb
+++ web.rb
@@ -13,31 +13,36 @@ use OmniAuth::Builder do
 end
 
 get "/" do
-  <<-HTML
-    <a href='/auth/heroku'>Sign in with Heroku</a>
+  if !session[:heroku_oauth_token]
+    <<-HTML
+    To deploy this app in your Heroku account please, <a href='/auth/heroku'>Sign in with Heroku</a>
+    HTML
+  else
+    <<-HTML
+    Hello #{CGI.escapeHTML(user_name)}, Provide your deployment details below
+    <form name="input" action="/deploy" method="get">
+      URL to source tarball: <input type="text" name="source_url"><br>
+      Last name: <input type="text" name="lastname">
+      <input type="submit" value="Submit">
+    </form>
   HTML
 end
+
+get "/deploy" do
+  <<-HTML
+  I am going to deploy now!
+  HTML
+end
+
 
 get "/auth/heroku/callback" do
   session[:heroku_oauth_token] =
     request.env["omniauth.auth"]["credentials"]["token"]
-  redirect "/user"
-end
-
-get "/user" do
-  if !session[:heroku_oauth_token]
-    redirect("/")
-  else
-    api = Excon.new(ENV["HEROKU_API_URL"] || "https://api.heroku.com",
+  api = Excon.new(ENV["HEROKU_API_URL"] || "https://api.heroku.com",
       headers: { "Authorization" => "Bearer #{session[:heroku_oauth_token]}" },
       ssl_verify_peer: ENV["SSL_VERIFY_PEER"] != "false")
     res = api.get(path: "/account", expects: 200)
-    user_email = MultiJson.decode(res.body)["email"]
-
-    <<-HTML
-      Hi #{CGI.escapeHTML(user_email)}
-    HTML
-  end
+    user_name = MultiJson.decode(res.body)["name"]
 end
 
 get "/getting-started" do
